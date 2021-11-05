f44c1003a438926bb894085e4af9571ff2ccb682
Jonathan <jonxplod@outlook.fr>
2021-11-05 09:38:45 +0100

    f

---
+++ main.js
@@ -0,0 +1,9 @@
+// 1 
+let exo1 = document.getElementsByClassName("tartine")[0];
+console.log(exo1);
+// 2
+let exo2 = document.getElementById("toast");
+console.log(exo2);
+// 3
+let exo3 = document.getElementsByClassName("tartine")[1];
+console.log(exo3);
